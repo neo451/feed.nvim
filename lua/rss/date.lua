9c8b278a3d10cf64d82eeca19cd90d6b574cbbd7
n451 <2020200706@ruc.edu.cn>
2024-08-28 22:16:21 +0800

    search module, date module

---
+++ lua/rss/date.lua
@@ -0,0 +1,80 @@
+local M = {}
+---@class rss.date
+---@field year number
+---@field month number
+---@field day number
+---@field hour number
+---@field min number
+---@field sec number
+---@field from_now fun():number
+
+local date = { __class = "date" }
+date.__index = date
+
+function date:from_now()
+	return os.time() - os.time(self)
+end
+
+function date:__lt(other)
+	return os.time(self) < os.time(other)
+end
+
+function date:__gt(other)
+	return os.time(self) > os.time(other)
+end
+
+function date:__tostring()
+	return ("%d-%02d-%02d"):format(self.year, self.month, self.day)
+end
+
+function date:days_ago(num)
+	return M.new(os.date("*t", os.time(self) - num * 24 * 60 * 60))
+end
+
+local months =
+	{ Jan = 1, Feb = 2, Mar = 3, Apr = 4, May = 5, Jun = 6, Jul = 7, Aug = 8, Sep = 9, Oct = 10, Nov = 11, Dec = 12 }
+
+---@param osdate table
+---@return rss.date
+function M.new(osdate)
+	return setmetatable(osdate, date)
+end
+
+---@type rss.date
+M.today = M.new(os.date("*t"))
+
+---@param str string
+---@return rss.date
+function M.new_from_str(str)
+	local a, b, c = str:match("(%d+)-(%w+)-(%w+)")
+	if not tonumber(b) then
+		--- TODO: year(s) ago
+		return M.today[b .. "_ago"](M.today, a)
+	end
+	return M.new({ year = a, month = b, day = c })
+end
+
+---@param str string
+---@return rss.date
+function M.new_from_entry(str)
+	local weekday, day, month, year, hour, min, sec, zone =
+		str:match("([%a]+), (%d+) ([%a]+) (%d+) (%d+):(%d+):(%d+) ([%+%-]%d+)")
+	year, month, day = tonumber(year), tonumber(months[month]), tonumber(day)
+	hour, min, sec = tonumber(hour), tonumber(min), tonumber(sec)
+	return M.new({ year = year, month = month, day = day, hour = hour, min = min, sec = sec })
+end
+
+---@param str string
+---@return ... rss.date
+function M.parse_date_filter(str)
+	local sep = string.find(str, "%-%-")
+	if not sep then
+		str = string.sub(str, 2, #str)
+		return { after = M.new_from_str(str) }, M.today
+	else
+		local start, stop = string.sub(str, 2, sep - 1), string.sub(str, sep + 2, #str)
+		return M.new_from_str(start), M.new_from_str(stop)
+	end
+end
+
+return M
---
+++ lua/rss/search.lua
@@ -0,0 +1,93 @@
+local lpeg = vim.lpeg
+local ut = require("rss.utils")
+local config = require("rss.config")
+local date = require("rss.date")
+
+local M = {
+	---"List of the entries currently on display."
+	---@type rss.entry[]
+	entries = {},
+	--- TODO: "List of the entries currently on display."
+	filter_history = {},
+	---@type integer
+	last_update = 0,
+	---List of functions to run immediately following a search buffer update.
+	---@type function[]
+	update_hook = config.search.update_hook, -- TODO:
+	sort_order = config.search.sort_order, -- TODO:
+}
+
+--- 1. date tag, compare with current date
+--- 2. = : title tag, fuzzy, regular expr
+--- 3. search tag + / -
+
+---@class rss.query
+---@field after rss.date
+---@field before rss.date
+---@field must_have rss.pattern #+
+---@field must_not_have rss.pattern #-
+---@field matches rss.pattern
+---@field not_matches rss.pattern
+---@field feeds string
+---@field not_feeds string
+---@field limit string -- TODO:
+---@field re rss.pattern #=
+
+---@alias rss.pattern vim.regex | vim.lpeg.Pattern | string # regex
+
+local filter_symbols = {
+	["+"] = "must_have",
+	["-"] = "must_not_have",
+	["="] = "re",
+	["@"] = "date",
+}
+
+---@param str string
+---@return rss.query
+function M.parse_query(str)
+	local query = { must_have = {}, must_not_have = {} }
+	for q in vim.gsplit(str, " ") do
+		-- print(q)
+		local kind = filter_symbols[q:sub(1, 1)]
+		if kind == "date" then
+			query.after, query.before = date.parse_date_filter(q)
+		elseif kind == "must_have" then
+			table.insert(query.must_have, q:sub(2))
+		elseif kind == "must_not_have" then
+			table.insert(query.must_not_have, q:sub(2))
+		end
+	end
+	return query
+end
+
+--- TODO: see lpeg.re.lua
+
+---check if a valid pattern
+---@param str any
+---@return boolean
+function M.valid_pattern(str)
+	if vim.lpeg.type(str) == "pattern" then
+		return true
+	else
+		local ok, obj = pcall(vim.regex, str)
+		if ok and tostring(obj) == "<regex>" then
+			return true
+		end
+	end
+	return false
+end
+
+---@param query rss.query
+---@return fun(entry: rss.entry): boolean
+function Query(query)
+	return function(entry)
+		return true
+	end
+end
+
+---@param entries rss.entry[]
+---@param query rss.query
+---@return rss.entry[]
+local function filter(entries, query) end
+
+return M
