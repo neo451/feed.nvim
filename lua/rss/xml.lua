8b347be8ea222a76a7da85cf19eb2acd2e4a31fe
n451 <2020200706@ruc.edu.cn>
2024-08-31 00:16:31 +0800

    cleanup xml impl

--- lua/rss/xml.lua
+++ lua/rss/xml.lua
@@ -16,94 +16,87 @@ local alnum = lpeg.alnum
 local punct = lpeg.punct
 
 local ws = S(" \t\n\r") ^ 0
-
-local id = function(a)
-	return a
-end
-
 local text = (1 - P("<")) ^ 1
 
-local name = (alnum - punct) ^ 1 / id -- TODO: check spec
-local quoted_string = P('"') * ((1 - P('"')) ^ 0 / id) * P('"')
+local name = C((alnum - punct) ^ 1) -- TODO: check spec
+local quoted_string = P('"') * C((1 - P('"')) ^ 0) * P('"')
 local kv = ws * name * "=" * quoted_string * ws
 
-local end_tag = P("</") * name * P(">") / id
+local end_tag = P("</") * name * P(">")
 
-local start_tag = P("<")
-		* name
-		* kv ^ 0
-		* P(">")
-		/ function(T, ...)
-			local tab = { ... }
-			if #tab == 0 then
-				return T
+local function parse_start_tag(T, ...)
+	local tab = { ... }
+	if #tab == 0 then
+		return T
+	end
+	local t = {}
+	t[T] = {}
+	for i = 1, select("#", ...) do
+		local k, v = select(i, ...)
+		t[T][k] = v
+	end
+	return t
+end
+
+local function parse_document(...)
+	if select("#", ...) == 1 then
+		return ...
+	end
+	return { ... }
+end
+
+local function parse_content(...)
+	if select("#", ...) == 1 then
+		return ...
+	end
+	local acc = {}
+	local last_key = nil
+	for _, v in pairs({ ... }) do
+		local key = vim.tbl_keys(v)[1]
+		if key == last_key then
+			if type(acc[key]) ~= "table" then
+				acc[key] = { acc[key] }
 			end
-			local t = {}
-			t[T] = {}
-			for i = 1, select("#", ...) do
-				local k, v = select(i, ...)
-				t[T][k] = v
+			for _, val in pairs(v) do
+				table.insert(acc[key], val)
 			end
-			return t
+		else
+			acc = vim.tbl_deep_extend("keep", acc, v)
 		end
+		last_key = key
+	end
+	return acc
+end
+
+local function parse_element(T, ele_or_text, _)
+	if type(T) == "table" then
+		if type(ele_or_text) == "table" then
+			return vim.tbl_extend("keep", T, ele_or_text)
+		else
+			-- TODO: possible?? if ele_or_text then end
+			return T
+		end
+	end
+	return { [T] = ele_or_text }
+end
+
+local start_tag = P("<") * name * kv ^ 0 * P(">") / parse_start_tag
+
+local CData = C((1 - P("]]>")) ^ 0)
+local CDSect = "<![CDATA[" * CData * "]]>"
+local XMLDecl = P([[<?xml version="1.0" encoding="UTF-8"?>]]) --HACK:
 
 local element = V("element")
 local content = V("content")
-
-local CData = (1 - P("]]>")) ^ 0 / id
-local CDSect = "<![CDATA[" * CData * "]]>"
-local XMLDecl = P([[<?xml version="1.0" encoding="UTF-8"?>]]) --HACK:
--- "<?xml" * text * "?>"
-
 local grammar = {
 	[1] = "document",
-
-	document = ws * XMLDecl ^ -1 * ws * element ^ 1 * ws / function(...)
-		if select("#", ...) == 1 then
-			return ...
-		end
-		return { ... }
-	end,
-	content = ws * (text + CDSect + (element ^ 0)) * ws / function(...)
-		if select("#", ...) == 1 then
-			return ...
-		end
-		local acc = {}
-		local last_key = nil
-		for _, v in pairs({ ... }) do
-			local key = vim.tbl_keys(v)[1]
-			if key == last_key then
-				if type(acc[key]) ~= "table" then
-					acc[key] = { acc[key] }
-				end
-				for k, val in pairs(v) do
-					table.insert(acc[key], val)
-				end
-			else
-				acc = vim.tbl_deep_extend("keep", acc, v)
-			end
-			last_key = key
-		end
-		return acc
-	end,
-	element = ws
-			* start_tag
-			* content
-			* end_tag
-			/ function(T, ele_or_text, _)
-				if type(T) == "table" then
-					if type(ele_or_text) == "table" then
-						return vim.tbl_extend("keep", T, ele_or_text)
-					else
-						-- TODO: possible?? if ele_or_text then end
-						return T
-					end
-				end
-				return { [T] = ele_or_text }
-			end
-			* ws,
+	document = ws * XMLDecl ^ -1 * ws * element ^ 1 * ws / parse_document,
+	content = ws * (text + CDSect + (element ^ 0)) * ws / parse_content,
+	element = ws * start_tag * content * end_tag / parse_element * ws,
 }
 
+---@param s string
+---@return table
 function M.parse(s)
 	return Ct(C(grammar)):match(s)[2]
 end
