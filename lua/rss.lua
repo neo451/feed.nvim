e69902f9084a561b3e09166185f1f4e16979f503
n451 <2020200706@ruc.edu.cn>
2024-09-02 21:09:26 +0800

    modify cleaner rss.lua

--- lua/rss.lua
+++ lua/rss.lua
@@ -7,52 +7,6 @@ local actions = require "rss.actions"
 
 local M = {}
 
-local autocmds = {}
-
--- function autocmds.load_opml(file)
--- 	local feeds = opml.parse_opml(file)
--- 	for _, feed in ipairs(feeds) do
--- 		local item = feed._attr
--- 		local title = item.title
--- 		local xmlUrl = item.xmlUrl
--- 		config.feeds[title] = xmlUrl
--- 	end
--- end
---
--- actions.load_opml("/home/n451/Plugins/rss.nvim/lua/list.opml")
-
-function autocmds.list_feeds()
-   print(vim.inspect(vim.tbl_values(config.feeds)))
-end
-
-function autocmds.update()
-   for i, link in ipairs(config.feeds) do
-      fetch.update_feed(link, #config.feeds, i)
-   end
-end
-
--- TODO: autocomp names/url when using usrcmd
--- function autocmds.update_feed(name)
--- 	fetch.update_feed(config.feeds[name], name, 1, 1)
--- end
-
-vim.api.nvim_create_user_command("Rss", function(opts)
-   if #opts.fargs > 1 then -- TODO:
-      error "too much args!"
-   else
-      autocmds[opts.args]()
-   end
-end, { nargs = 1 })
-
--- vim.api.nvim_create_autocmd("VimLeavePre", {
---    pattern = "*.md",
---    callback = function()
---       print "leave!"
---       db:save()
---       -- autocmds.update()
---    end,
--- })
-
 local function prepare_bufs()
    render.buf = {
       index = vim.api.nvim_create_buf(false, true),
@@ -76,7 +30,7 @@ function M.setup(user_config)
    config.og_colorscheme = vim.cmd "colorscheme" --TODO:??
    prepare_bufs()
 
-   -- vim.keymap.set("n", "<leader>rt", telescope.show_telescope, { desc = "Show [R]ss feed in [T]elescope" })
+   vim.keymap.set("n", "<leader>rt", "<cmd>Telescope rss<cr>", { desc = "Show [R]ss feed in [T]elescope" })
    vim.keymap.set("n", "<leader>rs", render.show_index, { desc = "Show [R][s]s feed" })
 end
 
