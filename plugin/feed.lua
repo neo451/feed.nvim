42f9dc516ef1ee6a24f8d1b475847c0e55b012ff
n451 <2020200706@ruc.edu.cn>
2024-09-04 21:45:06 +0800

    add plugin/feed.lua

---
+++ plugin/feed.lua
@@ -0,0 +1,20 @@
+local config = require "feed.config"
+require("feed.db").check_dir(config.db_dir)
+
+-- TODO: enough for now, very few actions
+vim.api.nvim_create_user_command("Rss", function(opts)
+   if #opts.fargs == 0 then
+      require("feed.render").show_index()
+   else
+      require("feed.commands").load_command(opts.fargs)
+   end
+end, {
+   nargs = "*",
+   complete = function(_, line)
+      local cmds_list = vim.tbl_keys(require("feed.commands").cmds)
+      local l = vim.split(line, "%s+")
+      return vim.tbl_filter(function(val)
+         return vim.startswith(val, l[#l])
+      end, cmds_list)
+   end,
+})
