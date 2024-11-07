1ec96e55faceb6d1e0f213af0c36eb259c553bbd
n451 <2020200706@ruc.edu.cn>
2024-11-07 22:12:30 +0800

    chore: simple build, just load nvim-treesitter...

--- lazy.lua
+++ lazy.lua
@@ -4,6 +4,10 @@ return {
    { "neo451/treedoc.nvim", lazy = true },
    {
       "neo451/feed.nvim",
+      dependencies = {
+         "nvim-treesitter/nvim-treesitter",
+      },
+      build = ":TSInstall xml html markdown",
       opts = {},
    },
 }
