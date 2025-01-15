89089110a9620c10e928e4a09f1d995f90ccfe6a
n451 <2020200706@ruc.edu.cn>
2025-01-15 16:43:14 +0800

    build: use lazy's opt table to make sure xml parser installed

--- lazy.lua
+++ lazy.lua
@@ -5,6 +5,10 @@ return {
       "neo451/feed.nvim",
       dependencies = {
          "nvim-treesitter/nvim-treesitter",
+         opts = function(_, opts)
+            opts.ensure_installed = opts.ensure_installed or {}
+            table.insert(opts.ensure_installed, "xml")
+         end,
       },
       opts = {},
    },
