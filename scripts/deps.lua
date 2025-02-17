ad95e7e08c251767f3af6e4663df02b126528b5c
n451 <2020200706@ruc.edu.cn>
2025-02-17 14:18:37 +0800

    ci: proper install ts-xml for ci

--- scripts/deps.lua
+++ scripts/deps.lua
@@ -8,10 +8,13 @@ local add = MiniDeps.add
 add({
    source = "nvim-treesitter/nvim-treesitter",
 })
-require("nvim-treesitter.configs").setup({
-   ensure_installed = { "xml", "html", "markdown" },
-})
 
+vim.cmd.TSInstall("xml")
+
+-- require("nvim-treesitter.configs").setup({
+--    ensure_installed = { "xml", "html", "markdown" },
+-- })
+--
 add({
    source = "gregorias/coop.nvim",
 })
--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,7 +1,7 @@
 -- Add current directory to 'runtimepath' to be able to use 'lua' files
-vim.cmd [[let &rtp.=','.getcwd()]]
+vim.cmd([[let &rtp.=','.getcwd()]])
 
-for f in vim.fs.dir "./deps/pack/deps/opt/" do
+for f in vim.fs.dir("./deps/pack/deps/opt/") do
    vim.opt.runtimepath:append("," .. "./deps/pack/deps/opt/" .. f)
 end
 
@@ -9,12 +9,15 @@ end
 if #vim.api.nvim_list_uis() == 0 then
    -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
    -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
-   vim.cmd "set rtp+=deps/mini.nvim"
+   vim.cmd("set rtp+=deps/mini.nvim")
+   path = vim.api.nvim_get_runtime_file("parser/xml.so", true)[1]
+
+   print(path)
+
+   vim.treesitter.language.add("xml", {
+      path = vim.api.nvim_get_runtime_file("parser/xml.so", false)[1],
+   })
 
    -- Set up 'mini.test'
    require("mini.test").setup()
 end
-
-vim.treesitter.language.add("xml", {
-   path = vim.api.nvim_get_runtime_file("parser/xml.so", false)[1],
-})
