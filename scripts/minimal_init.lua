064ca9659f66b3f65d711395e0efb7ea1737884f
n451 <2020200706@ruc.edu.cn>
2025-02-21 01:59:32 +0800

    test: update makefile

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,19 +1,12 @@
 -- Add current directory to 'runtimepath' to be able to use 'lua' files
 vim.cmd([[let &rtp.=','.getcwd()]])
 
-for f in vim.fs.dir("./deps/pack/deps/opt/") do
-   vim.opt.runtimepath:append("," .. "./deps/pack/deps/opt/" .. f)
-end
-
 -- Set up 'mini.test' only when calling headless Neovim (like with `make test`)
 if #vim.api.nvim_list_uis() == 0 then
    -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
    -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
    vim.opt.rtp:append("deps/mini.nvim")
    vim.opt.rtp:append("~/.luarocks/lib/luarocks/rocks-5.1/tree-sitter-xml/*/")
-   path = vim.api.nvim_get_runtime_file("parser/xml.so", true)[1]
-
-   print(path)
 
    vim.treesitter.language.add("xml", {
       path = vim.api.nvim_get_runtime_file("parser/xml.so", false)[1],
