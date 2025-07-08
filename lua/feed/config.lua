5e426728843e5edce856b32815317ac0790e83f2
neo451 <412444506@qq.com>
2025-07-08 21:13:42 +0800

    fix: remove opts.keys, just let users change with autocmds

--- lua/feed/config.lua
+++ lua/feed/config.lua
@@ -50,9 +50,6 @@
 ---@field search? feed.searchOpts
 ---@field protocol? feed.protocolOpts
 ---@field options? { entry: { wo: vim.wo|{}, bo: vim.bo|{} }, index: { wo: vim.wo|{}, bo: vim.bo|{} } }
----@field keys? { index: feed.key[], entry: feed.key[] }
-
----@alias feed.key table<string | number, string | function>
 
 local formats = {}
 
@@ -260,38 +257,6 @@ default = {
 
    feeds = {},
 
-   keys = {
-      index = {
-         { "q", "<cmd>Feed quit<cr>" },
-         { "?", "<cmd>Feed hints<cr>" },
-         { ".", "<cmd>Feed dot<cr>" },
-         { "u", "<cmd>Feed undo<cr>" },
-         { "<C-r>", "<cmd>Feed redo<cr>" },
-         { "<M-CR>", "<cmd>Feed split<cr>" },
-         { "b", "<cmd>Feed browser<cr>" },
-         { "r", "<cmd>Feed refresh<cr>" },
-         { "R", "<cmd>Feed update<cr>" },
-         { "/", "<cmd>Feed search<cr>" },
-         { "Y", "<cmd>Feed yank_url<cr>" },
-         { "-", "<cmd>Feed untag<cr>" },
-         { "+", "<cmd>Feed tag<cr>" },
-         { "<cr>", "<cmd>Feed entry<cr>" },
-      },
-      entry = {
-         { "q", "<cmd>Feed quit<cr>" },
-         { "?", "<cmd>Feed hints<cr>" },
-         { "Y", "<cmd>Feed yank_url<cr>" },
-         { "b", "<cmd>Feed browser<cr>" },
-         { "}", "<cmd>Feed next<cr>" },
-         { "{", "<cmd>Feed prev<cr>" },
-         { "/", "<cmd>Feed search<cr>" },
-         { "-", "<cmd>Feed untag<cr>" },
-         { "+", "<cmd>Feed tag<cr>" },
-         { "f", "<cmd>Feed full<cr>" },
-         { "r", "<cmd>Feed urlview<cr>" },
-      },
-   },
-
    options = {
       index = {
          wo = {
@@ -409,7 +374,6 @@ local function validate(cfg)
       progress = "table",
       data = "table",
       feeds = "table",
-      keys = "table",
       icons = "table",
    }
    for k, v in pairs(path) do
@@ -424,15 +388,7 @@ end
 ---@param config feed.config
 function M.resolve(config)
    config = config or {}
-   config.keys = config.keys or {}
-   local index_keys, entry_keys = config.keys.index, config.keys.entry
-
-   config.keys.index, config.keys.entry = nil, nil
-
    M.config = vim.tbl_deep_extend("keep", config, default)
-
-   vim.list_extend(M.config.keys.index, index_keys or {})
-   vim.list_extend(M.config.keys.entry, entry_keys or {})
    -- validate(M.config)
 end
 
