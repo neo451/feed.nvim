5a3834998e7c0fb75ccd94fa717e5bb0f6953521
neo451 <412444506@qq.com>
2026-03-31 21:36:18 +0100

    fix: progress_message

---
+++ home/n451/.local/share/nvim/feed/feeds.lua
@@ -0,0 +1,122 @@
+return {
+  ["https://andrewkelley.me/rss.xml"] = {
+    desc = "My personal website - thoughts, project demos, research.",
+    etag = '"d95403b13a9eba9af9bf84f972b777fa"',
+    htmlUrl = "https://andrewkelley.me/",
+    last_modified = "Wed, 29 Oct 2025 20:01:05 GMT",
+    tags = { "blog", "zig" },
+    title = "Andrew Kelley",
+    version = "rss20"
+  },
+  ["https://dotfyle.com/this-week-in-neovim/rss.xml"] = {
+    desc = "A weekly newsletter with updates from the Neovim ecosystem, including Neovim core and the plugin community. New plugins RSS: https://dotfyle.com/neovim/plugins/rss.xml",
+    htmlUrl = "https://dotfyle.com/this-week-in-neovim",
+    last_modified = "Sun, 29 Mar 2026 17:32:19 GMT",
+    tags = { "neovim", "news" },
+    title = "This Week in Neovim",
+    version = "rss20"
+  },
+  ["https://feeds.feedburner.com/ruanyifeng"] = {
+    desc = "Ruan YiFeng's Blog",
+    htmlUrl = "http://www.ruanyifeng.com/blog/",
+    last_modified = "Fri, 27 Mar 2026 01:47:34 GMT",
+    tags = { "blog" },
+    title = "阮一峰的网络日志",
+    version = "rss20"
+  },
+  ["https://feeds.megaphone.fm/BVLLC2163264914"] = {
+    desc = "On Mondays, Jon Stewart hosts The Daily Show, but on Thursdays, he's back in your ears with The Weekly Show -- a podcast featuring in-depth conversations with a range of special guests. From experts and advocates, to stakeholders and thought leaders, we discuss the challenges, changes, and ideas that are shaping our world.",
+    htmlUrl = "https://art19.com/shows/jon-stewart",
+    last_modified = "Wed, 25 Mar 2026 07:30:00 GMT",
+    tags = { "podcast" },
+    title = "The Weekly Show with Jon Stewart",
+    version = "rss20"
+  },
+  ["https://feeds.simplecast.com/dHoohVNH"] = {
+    desc = "After 25 years at the Late Night desk, Conan realized that the only people at his holiday party are the men and women who work for him. Over the years and despite thousands of interviews, Conan has never made a real and lasting friendship with any of his celebrity guests. So, he started a podcast to do just that. Deeper, unboundedly playful, and free from FCC regulations, Conan O’Brien Needs a Friend is a weekly opportunity for Conan to hang out with the people he enjoys most and perhaps find some real friendship along the way.  Subscribe to SiriusXM Podcasts+ to listen to new episodes of Conan O'Brien Needs a Friend ad-free and a whole week early. Start a free trial now on Apple Podcasts or by visiting siriusxm.com/podcastsplus.",
+    etag = '"96c01f6c885ad01cad445ac0935b042b"',
+    htmlUrl = "https://www.siriusxm.com",
+    last_modified = "Mon, 30 Mar 2026 04:05:31 GMT",
+    tags = { "podcast" },
+    title = "Conan O’Brien Needs A Friend",
+    version = "rss20"
+  },
+  ["https://neo451.github.io/blog/index.xml"] = {
+    desc = "Where I rant about tech and pop culture stuff",
+    etag = '"6938a96a-2d80e"',
+    htmlUrl = "https://neo451.github.io/blog/",
+    last_modified = "Tue, 09 Dec 2025 22:57:46 GMT",
+    tags = { "blog" },
+    title = "zztopia",
+    version = "rss20"
+  },
+  ["https://neovim.io/news.xml"] = {
+    desc = "Recent content on Neovim",
+    etag = 'W/"69cbced8-aac4"',
+    htmlUrl = "https://neovim.io/",
+    last_modified = "Tue, 31 Mar 2026 13:40:40 GMT",
+    tags = { "neovim", "news" },
+    title = "Neovim",
+    version = "rss20"
+  },
+  ["https://obsidian.md/changelog.xml"] = {
+    desc = "Follow new releases of Obsidian for mobile and desktop.",
+    etag = 'W/"69c18579-e06bd"',
+    htmlUrl = "https://obsidian.md/changelog.xml",
+    last_modified = "Mon, 23 Mar 2026 18:24:57 GMT",
+    tags = { "news" },
+    title = "Obsidian Changelog",
+    version = "atom10"
+  },
+  ["https://pawelgrzybek.com/feed.xml"] = {
+    desc = "I'm a software developer from Poland living in Northampton, UK. I'm a web standards enthusiast, accessibility advocate, and simplicity guardian.",
+    etag = '"76fb1db241afb22daae59967291d14ff-ssl"',
+    htmlUrl = "https://pawelgrzybek.com/",
+    tags = { "blog", "news" },
+    title = "pawelgrzybek.com",
+    version = "rss20"
+  },
+  ["https://www.bluesnews.com/news/news_1_0.rdf"] = {
+    desc = "Up to the minute news on PC games, video games, computers, and related technologies.",
+    etag = '"548c7c-d640-64e545e02d44b"',
+    htmlUrl = "https://www.bluesnews.com",
+    last_modified = "Tue, 31 Mar 2026 16:22:01 GMT",
+    tags = { "gaming", "news" },
+    title = "Blue's News",
+    version = "rss10"
+  },
+  ["https://www.jeffquast.com/index.xml"] = {
+    desc = "Recent content on Articles",
+    etag = '"f8b6d4f6d96c5a652d91c1a8f758d788"',
+    htmlUrl = "https://www.jeffquast.com/",
+    last_modified = "Tue, 03 Mar 2026 15:59:03 GMT",
+    tags = { "blog" },
+    title = "Articles",
+    version = "rss20"
+  },
+  ["https://www.openmymind.net/atom.xml"] = {
+    desc = "Programming blog exploring Zig, Elixir, Go, Testing, Design and Performance",
+    etag = '"69afb27e-2ddec"',
+    htmlUrl = "https://www.openmymind.net/",
+    last_modified = "Tue, 10 Mar 2026 05:56:14 GMT",
+    tags = { "blog" },
+    title = "openmymind.net",
+    version = "atom10"
+  },
+  ["https://ziglang.org/news/index.xml"] = {
+    desc = "Recent news",
+    etag = '"69cc217f-1cbf"',
+    htmlUrl = "https://ziglang.org/news/",
+    last_modified = "Tue, 31 Mar 2026 19:33:19 GMT",
+    tags = { "news", "zig" },
+    title = "Ziglang.org News",
+    version = "rss20"
+  },
+  ["r/neovim"] = {
+    desc = "Neovim is a hyperextensible Vim-based text editor. Learn more at neovim.io.",
+    htmlUrl = "https://www.reddit.com/r/neovim",
+    tags = { "neovim", "news" },
+    title = "Neovim",
+    version = "atom10"
+  }
+}
--- lua/feed/ui/progress.lua
+++ lua/feed/ui/progress.lua
@@ -1,48 +1,25 @@
-local config = require("feed.config")
-local _, MiniNotify = pcall(require, "mini.notify")
-local _, SnacksNotifier = pcall(require, "snacks.notify")
-local SnacksNotify
-if SnacksNotifier then
-   SnacksNotify = SnacksNotifier.notify
-end
-
 ---@class feed.progress
 ---@field total integer
 ---@field count integer
 ---@field t integer
----@field update fun(self: feed.progress, message: string)
----@field finish function
----@field new function
----@field backend feed.progress
+---@field progress vim.api.keyset.echo_opts
 local M = {}
 M.__index = M
-
-local backends = setmetatable({}, {
-   __index = function()
-      return {
-         new = function() end,
-         update = function() end,
-         finish = function() end,
-      }
-   end,
-})
 
 function M.new(total)
    local ret = {}
    ret.total = total
    ret.count = 0
    ret.t = os.time()
-   ret.backend = backends[config.progress.backend]
-   ret.backend:new()
-   ret.__index = ret
-   setmetatable(ret.backend, ret)
    return setmetatable(ret, M)
 end
 
 function M:finish()
    local msg = ("Fetched update in %ds"):format(os.time() - self.t)
-   self.backend:finish(msg)
    vim.g.feed_progress = msg
+   vim.schedule(function()
+      vim.api.nvim_echo({ { msg } }, true, self.progress)
+   end)
    vim.defer_fn(function()
       vim.g.feed_progress = nil
    end, 2000)
@@ -51,63 +28,24 @@ end
 function M:update(msg)
    vim.g.feed_progress = msg
    self.count = self.count + 1
-   self.backend:update(msg)
-end
 
-local fidget = {}
+   local progress = {
+      kind = "progress",
+      status = "running",
+      percent = math.floor(self.count / self.total * 100),
+      title = "feed.nvim update",
+      source = "feed.nvim",
+   }
 
-function fidget:new(msg)
-   local _, progress = pcall(require, "fidget.progress")
-   self.handle = progress.handle.create({
-      title = "Feed update",
-      message = msg,
-      percentage = 0,
-   })
-end
+   vim.schedule(function()
+      progress.id = vim.api.nvim_echo({ { msg } }, true, progress)
+   end)
 
-function fidget:update(msg)
-   self.handle.percentage = self.handle.percentage + 100 / self.total
-   self.handle.message = msg
-end
+   self.progress = progress
 
-function fidget:finish(msg)
-   self.handle.message = msg
-   self.handle:finish()
-end
-
-local mini = {}
-
-function mini:new() end
-
-function mini:update(msg)
-   if not self.id then
-      self.id = MiniNotify.add(msg, "INFO", "Title")
-   else
-      MiniNotify.update(self.id, { msg = msg })
+   if self.count == self.total then
+      self:finish()
    end
 end
-
-function mini:finish(msg)
-   MiniNotify.remove(self.id)
-   self.id = nil
-   local opts = { INFO = { duration = 1000 } }
-   MiniNotify.make_notify(opts)(msg)
-end
-
-local snacks = {}
-
-function snacks:new() end
-
-function snacks:update(msg)
-   SnacksNotify(msg, { id = "feed" })
-end
-
-function snacks:finish(msg)
-   SnacksNotify(msg, { id = "feed" })
-end
-
-backends.fidget = fidget
-backends.snacks = snacks
-backends.mini = mini
 
 return M
